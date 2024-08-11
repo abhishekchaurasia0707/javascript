7c414691f594140d33b912709376080594bd4735
Abhishek Kumar <125857281+abhishekchaurasia0707@users.noreply.github.com>
2024-08-11 07:04:41 +0000

    gone

---
+++ 01_start/02datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all the JS code as newest version
+// alert(3+3) //we are using nodejs,not browser
+
+//datatype in javascript 
+let name="abhishek" //string data
+let age=18 //int data
+let isLoggedIn=false //boolean data
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null =>standlone value
+//undefined =>
+//symbol =>unique
+
+
+//object
+console.log(typeof "abhishek");
+console.log(typeof age);
+console.log(typeof undefined) //output will be undefined
+console.log(typeof null) //output will be object
