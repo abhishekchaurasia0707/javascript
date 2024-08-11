626e2ed057f28d9837d7b04c3e77d55d5dbbefad
Abhishek Kumar <125857281+abhishekchaurasia0707@users.noreply.github.com>
2024-08-11 05:44:02 +0000

    done

---
+++ 01_start/variable.js
@@ -0,0 +1,18 @@
+const accountID=1234;
+let accountemail="abhishekiipcm9097@gmail.com";
+var accountPassword="34545";
+accountcenter="Jaipur";
+let accountstat;
+//agar javascript ke ander variable declare kar ke chhod dete h to javascript usko undefined manti hai...................
+console.table([accountID,accountemail,accountPassword,accountcenter,accountstat]);
+
+//accountID=1546;
+accountemail="abhishek9097indian@gmail.com";
+accountPassword="9097";
+accountcenter="Banglore";
+
+console.table([accountID,accountemail,accountPassword,accountcenter,accountstat]);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
